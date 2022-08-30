45b77666c37343b69e733d30753b4d1b9fb5ca3b
Tom Flídr <tomflidr@gmail.com>
2022-08-30 11:58:45 +0200

    release v5.1.6

--- src/MvcCore/Ext/Views/Helpers/Assets.php
+++ src/MvcCore/Ext/Views/Helpers/Assets.php
@@ -23,7 +23,7 @@ abstract class Assets extends \MvcCore\Ext\Views\Helpers\AbstractHelper {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.1.5';
+	const VERSION = '5.1.6';
 
 	/**
 	 * Default link group name
